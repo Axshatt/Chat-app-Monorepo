197e979ce7601fd95e14834586c06393111e7d11
Akshat <codepreneur.in@gmail.com>
2025-08-06 19:41:12 +0530

    added ui in ./packages/ui

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,16 @@
+interface PropType {
+    placeholders: string
+
+}
+
+export function TextInput({ placeholders }: PropType) {
+    return <input placeholder={placeholders} style={{
+        padding: 10,
+        margin: 10,
+        borderColor: "black",
+        borderWidth: 1,
+
+    }
+
+    }></input>
+}
